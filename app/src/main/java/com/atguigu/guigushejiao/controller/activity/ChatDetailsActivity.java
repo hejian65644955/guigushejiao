07724cfa23622ef9296fa2dd0af1833d250f1f2b
hejian65644955 <m15895420701@163.com>
2017-02-20 20:24:39 +0800

    群详情的监听

---
+++ app/src/main/java/com/atguigu/guigushejiao/controller/activity/ChatDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.guigushejiao.controller.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.atguigu.guigushejiao.R;
+
+public class ChatDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_details);
+    }
+}
